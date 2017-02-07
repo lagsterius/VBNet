22d33e450d95fc43b9cc05c4545fde8e78e76b07
lagsterius <lagsterius@gmail.com>
2017-02-07 23:29:50 +0300

    Добавлены проекты VB

---
+++ IPinfo/IPinfo/Form1.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.AxWinsock1 = New AxMSWinsockLib.AxWinsock()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.IP = New System.Windows.Forms.TextBox()
+        CType(Me.AxWinsock1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'AxWinsock1
+        '
+        Me.AxWinsock1.Enabled = True
+        Me.AxWinsock1.Location = New System.Drawing.Point(13, 227)
+        Me.AxWinsock1.Name = "AxWinsock1"
+        Me.AxWinsock1.OcxState = CType(resources.GetObject("AxWinsock1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWinsock1.Size = New System.Drawing.Size(28, 28)
+        Me.AxWinsock1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(13, 38)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(147, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Получить IP"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'IP
+        '
+        Me.IP.Location = New System.Drawing.Point(13, 12)
+        Me.IP.Name = "IP"
+        Me.IP.Size = New System.Drawing.Size(147, 20)
+        Me.IP.TabIndex = 2
+        Me.IP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Form1
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.ClientSize = New System.Drawing.Size(172, 73)
+        Me.Controls.Add(Me.IP)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.AxWinsock1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "Получение IP"
+        CType(Me.AxWinsock1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents AxWinsock1 As AxMSWinsockLib.AxWinsock
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents IP As System.Windows.Forms.TextBox
+
+End Class
---
+++ IPinfo/IPinfo/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Dim HTMLCode As String
+    Dim PocketLen As Integer
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        AxWinsock1.RemotePort = 80
+        AxWinsock1.RemoteHost = "ip-whois.net"
+        AxWinsock1.Connect()
+    End Sub
+
+    Private Sub AxWinsock1_CloseEvent(ByVal sender As Object, ByVal e As System.EventArgs) Handles AxWinsock1.CloseEvent
+        Me.Text = "Not Connected"
+        AxWinsock1.Close()
+    End Sub
+
+    Private Sub AxWinsock1_ConnectEvent(ByVal sender As Object, ByVal e As System.EventArgs) Handles AxWinsock1.ConnectEvent
+        Me.Text = "Connected"
+        AxWinsock1.SendData("GET " + "/ip.php" + " HTTP/1.0" + Chr(10) + Chr(10))
+    End Sub
+
+    Private Sub AxWinsock1_DataArrival(ByVal sender As Object, ByVal e As AxMSWinsockLib.DMSWinsockControlEvents_DataArrivalEvent) Handles AxWinsock1.DataArrival
+        Dim temp As String
+        temp = ""
+        PocketLen += AxWinsock1.BytesReceived
+        AxWinsock1.GetData(temp)
+        HTMLCode = HTMLCode + temp
+        If PocketLen > 7000 Then IP.Text = CutIP(HTMLCode)
+    End Sub
+    Function CutIP(ByVal HTML As String) As String
+        Dim p1, p2 As Integer
+        p1 = 1
+        p1 = InStr(HTML, "Ваш IP:")
+        If p1 = 0 Then
+            CutIP = "Не удалось получить IP"
+        Else
+            HTML = Mid(HTML, p1, Len(HTML) - p1)
+            p2 = InStr(HTML, "</h2>")
+            CutIP = Trim(Mid(HTML, 8, p2 - 8))
+        End If
+    End Function
+End Class
